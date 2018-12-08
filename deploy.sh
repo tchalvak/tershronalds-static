d5401a733f0e31995b45f61aff4a3df6e60d64a3
Roy R <tchalvakspam@gmail.com>
2018-12-08 04:26:11 -0500

    Update contact and tel details.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ echo "...syncing..."
 aws s3 sync ./public/ s3://tershronalds-splash/ --profile=bitdog --exclude ".git/*" --exclude "deploy.sh" --exclude "README.md" --delete
 echo "---------------------------------------------------------------------------------------"
 echo ""
-echo "Site should be uploaded at: https://splash.tershronalds.net or cloudfront https://dd4vxxwmvkwud.cloudfront.net "
+echo "Site should be uploaded at: https://tershronalds.com or cloudfront https://dd4vxxwmvkwud.cloudfront.net "
 echo "Done."
