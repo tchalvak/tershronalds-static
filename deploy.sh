199caf1752516fbd50bb2b6be80b42d43a3fd1fe
Roy R <tchalvakspam@gmail.com>
2019-04-03 13:48:40 -0400

    Update with facebook button,
    deploy creates invalidation.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "...syncing..."
 aws s3 sync ./public/ s3://tershronalds-splash/ --profile=bitdog --exclude ".git/*" --exclude "deploy.sh" --exclude "README.md" --delete
+aws cloudfront create-invalidation --distribution-id E1O0NPA1HYQUE2 --paths /index.html --profile bitdog
 echo "---------------------------------------------------------------------------------------"
 echo ""
 echo "Site should be uploaded at: https://tershronalds.com or cloudfront https://dd4vxxwmvkwud.cloudfront.net "
